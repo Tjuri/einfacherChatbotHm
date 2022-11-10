2257b6857c0d1f81d69f63d7e3c3ef9ab4a45fcd
Michael Eggers <mseggers@protonmail.ch>
2022-11-10 12:37:38 +0100

    add mvg-api. add intent to ask for travel time between two stations

--- actions/actions.py
+++ actions/actions.py
@@ -7,12 +7,15 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+from sys import displayhook
 from typing import Any, Text, Dict, List
 from pyparsing import nestedExpr
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+from . import mvg
+import json
 
 # NOTE(Michael): We could use this action to store the name in
 #                the TrackerStore (in memory database) or a persitent DB
@@ -41,6 +44,26 @@ class ActionUserName(Action):
             dispatcher.utter_message(" Du hast mir Deinen Namen nicht gesagt.")
         else:
             dispatcher.utter_message(' Du bist {}'.format(username))
+
+        return []
+
+class ActionMVG(Action):
+
+     def name(self) -> Text:
+         return "action_get_travel_time"
+
+     def run(self, dispatcher, tracker, domain):
+        from_station = tracker.get_slot("from_station")
+        to_station = tracker.get_slot("to_station")
+        if not from_station or not to_station :
+            dispatcher.utter_message("Diese Stationen habe ich nicht erkannt!")
+        else:
+            result = json.loads(mvg.handle_route(from_station, to_station))
+            print(result)
+            if "error" in result:
+                dispatcher.utter_message(result["error"])
+            else:
+                dispatcher.utter_message("Du brauchst exakt: {} Minuten. Gute Reise!".format(result["time_needed"]))
 
         return []
 
